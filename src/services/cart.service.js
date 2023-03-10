2d599b94ff0f1e13fa5a9a0c04a302709c68ec27
Mazhar-4321 <syedmazharali742@gmail.com>
2023-03-10 14:10:20 +0530

    [Mazhar]ADDED: Cart Service To The Project

---
+++ src/services/cart.service.js
@@ -0,0 +1,11 @@
+import BookStore from '../models/book.model';
+import Cart from '../models/cart.model'
+
+
+export const addBookToCart = async (userId, book) => {
+
+}
+
+export const removeBookFromCart = async (userId, book) => {
+
+}
